44893a74f6ddca8ac16e6328e2a24916efc67da2
Gazi Monir-Uz-Zaman <ciphercall@gmail.com>
2024-11-19 10:47:43 +0600

    Image
    drag and drop image upload implemented.

--- app/(pages)/mint/createCollectible/page.tsx
+++ app/(pages)/mint/createCollectible/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, DragEvent } from "react";
 import PushToMintCollectionButton from "@/components/Button/PushToMintCollectionButton";
 import Image from "next/image";
 
@@ -73,6 +73,23 @@ const CreateCollectiblePage = () => {
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
+    if (!file) return;
+
+    setSelectedImageName(file.name);
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setFormData((prevState) => ({
+        ...prevState,
+        imageUrl: reader.result as string,
+      }));
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const handleImageDrop = (event: DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files?.[0];
     if (!file) return;
 
     setSelectedImageName(file.name);
@@ -163,7 +180,11 @@ const CreateCollectiblePage = () => {
             <label htmlFor="imageUrl" className="mb-1 block font-medium">
               Image (JPEG, JPG, PNG)
             </label>
-            <div className="bg-gray-100 p-4 rounded-lg border border-dashed border-gray-300 text-center">
+            <div
+              className="bg-gray-100 p-4 rounded-lg border border-dashed border-gray-300 text-center"
+              onDragOver={(e) => e.preventDefault()}
+              onDrop={handleImageDrop}
+            >
               {formData.imageUrl ? (
                 <div className="flex flex-col items-center">
                   <Image
