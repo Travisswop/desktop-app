13018abf961d2930863322ce724091edfbf89399
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2024-11-29 12:22:05 +0600

    test

--- app/(pages)/smartsite/page.tsx
+++ app/(pages)/smartsite/page.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 // import SecondaryButton from "@/components/SecondaryButton";
 // import { Checkbox, useDisclosure } from "@nextui-org/react";
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 // import { AiOutlineSelect } from "react-icons/ai";
 // import { BsSend, BsSendFill } from "react-icons/bs";
 // import { IoDuplicateOutline } from "react-icons/io5";
 // import { TbTransfer } from "react-icons/tb";
-import { LiaFileMedicalSolid } from 'react-icons/lia';
-import Link from 'next/link';
-import ButtonList from '@/components/smartsite/ButtonList';
-import isUrl from '@/lib/isUrl';
-import SmartsiteSocialShare from '@/components/smartsite/socialShare/SmartsiteSocialShare';
-import { useUser } from '@/lib/UserContext';
-import { useDesktopUserData } from '@/components/tanstackQueryApi/getUserData';
-import SmartSitePageLoading from '@/components/loading/SmartSitePageLoading';
+import { LiaFileMedicalSolid } from "react-icons/lia";
+import Link from "next/link";
+import ButtonList from "@/components/smartsite/ButtonList";
+import isUrl from "@/lib/isUrl";
+import SmartsiteSocialShare from "@/components/smartsite/socialShare/SmartsiteSocialShare";
+import { useUser } from "@/lib/UserContext";
+import { useDesktopUserData } from "@/components/tanstackQueryApi/getUserData";
+import SmartSitePageLoading from "@/components/loading/SmartSitePageLoading";
 // import ButtonList from "@/components/smartsiteList/ButtonList";
 // import SmartSiteUrlShareModal from "@/components/ShareModal/SmartsiteShareModal";
 // import SmartsiteSocialShare from "@/components/SmartsiteSocialShare";
@@ -23,13 +23,15 @@ import SmartSitePageLoading from '@/components/loading/SmartSitePageLoading';
 const SmartsitePage = () => {
   const { user, loading, accessToken } = useUser();
 
+  console.log("user in shhh", user);
+
   const {
     data,
     error,
     isLoading,
     // isFetching,
     //refetch,
-  } = useDesktopUserData(user?._id, accessToken || '');
+  } = useDesktopUserData(user?._id, accessToken || "");
 
   if (loading) {
     return <SmartSitePageLoading />;
@@ -83,9 +85,7 @@ const SmartsitePage = () => {
                     className="rounded-full w-28 h-28"
                   />
                 </div>
-                <SmartsiteSocialShare
-                  profileUrl={microsite.profileUrl}
-                />
+                <SmartsiteSocialShare profileUrl={microsite.profileUrl} />
               </div>
 
               <div className="flex flex-col items-center gap-4">
@@ -93,21 +93,19 @@ const SmartsitePage = () => {
                   <h3 className="text-lg font-bold text-gray-700">
                     {microsite.name}
                   </h3>
-                  <p className="font-medium text-gray-500">
-                    {microsite.bio}
-                  </p>
+                  <p className="font-medium text-gray-500">{microsite.bio}</p>
                 </div>
                 <ButtonList
                   microsite={microsite}
-                  token={accessToken || ''}
-                  id={user?._id || ''}
+                  token={accessToken || ""}
+                  id={user?._id || ""}
                 />
               </div>
             </div>
           ))}
 
         <Link
-          href={'/smartsite/create-smartsite'}
+          href={"/smartsite/create-smartsite"}
           className="bg-white px-4 py-[4rem] rounded-xl shadow-small flex flex-col gap-6 items-center"
         >
           <div className="p-5 bg-gray-200 w-max rounded-full">
