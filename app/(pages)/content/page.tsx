73b1f500505b786303acbf601c0e3eb63d356390
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2024-11-30 20:58:32 +0600

    fix content issue

--- app/(pages)/content/page.tsx
+++ app/(pages)/content/page.tsx
@@ -1,50 +1,25 @@
-'use client';
-import ContentInfo from '@/components/content/Content';
-import { useDesktopUserData } from '@/components/tanstackQueryApi/getUserData';
-import { useUser } from '@/lib/UserContext';
-import { useFetchMediaData } from '@/lib/hooks/useFetchMediaData';
-import { useMemo } from 'react';
+"use client";
+// import ContentInfo from "@/components/content/Content";
+// import { useDesktopUserData } from "@/components/tanstackQueryApi/getUserData";
+import { useUser } from "@/lib/UserContext";
+// import { useFetchMediaData } from "@/lib/hooks/useFetchMediaData";
+// import { useMemo } from "react";
+import MainContent from "./mainContent";
 
 const Content: React.FC = () => {
   const { user, loading, accessToken } = useUser();
-
-  const {
-    data: desktopData,
-    error: desktopError,
-    isLoading: isDesktopLoading,
-  } = useDesktopUserData(user?._id || '', accessToken || '');
-
-  const { audioUrls, videoUrls } = useMemo(() => {
-    const audio =
-      desktopData?.microsites?.flatMap((microsite: any) =>
-        microsite.info?.audio?.length > 0 ? microsite.info.audio : []
-      ) || [];
-
-    const video =
-      desktopData?.microsites?.flatMap((microsite: any) =>
-        microsite.info.video?.length > 0 ? microsite.info.video : []
-      ) || [];
-
-    return { audioUrls: audio, videoUrls: video };
-  }, [desktopData?.microsites]);
-
-  const {
-    data: mediaData,
-    error: mediaError,
-    isLoading: isMediaLoading,
-  } = useFetchMediaData(
-    user?._id || '',
-    accessToken || '',
-    audioUrls,
-    videoUrls
-  );
+  if (loading) {
+    <p>Loading...</p>;
+  }
 
   return (
-    <ContentInfo
-      audioList={mediaData?.audio}
-      videoList={mediaData?.video}
-      isLoading={isDesktopLoading || isMediaLoading}
-    />
+    <div>
+      {loading ? (
+        "loading..."
+      ) : (
+        <MainContent id={user?._id} token={accessToken} />
+      )}
+    </div>
   );
 };
 
