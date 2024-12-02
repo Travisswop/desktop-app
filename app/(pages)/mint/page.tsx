e15b7ea1dc1d64e2673ccafa334dfc9875e54e50
Gazi Monir-Uz-Zaman <ciphercall@gmail.com>
2024-12-02 13:12:31 +0600

    Add NFT to this type button
    functionality modified

--- app/(pages)/mint/page.tsx
+++ app/(pages)/mint/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, ChangeEvent } from "react";
+import React, { useState, useEffect } from "react";
 import MintCart from "@/components/MintCart";
 import Link from "next/link";
 import PushToMintCollectionButton from "@/components/Button/PushToMintCollectionButton";
@@ -38,8 +38,6 @@ interface GroupedTemplatesByCollection {
 }
 
 const MintDashboard = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState("");
   const [mintData, setMintData] = useState<
     { data: GroupedTemplatesByCollection[] } | { noCollections: boolean } | null
   >(null);
@@ -80,6 +78,9 @@ const MintDashboard = () => {
     fetchData();
   }, [accessToken, waitForToken]);
 
+  const capitalizeFirstLetter = (str: string) =>
+    str.charAt(0).toUpperCase() + str.slice(1);
+
   if (loading) {
     return <HomePageLoading />;
   }
@@ -103,33 +104,6 @@ const MintDashboard = () => {
     );
   }
 
-  const handleSaveClick = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleModalClose = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleConfirmClick = () => {
-    const routes: { [key: string]: string } = {
-      Collectible: "/mint/createCollectible",
-      Coupon: "/mint/createCoupon",
-      Subscription: "/mint/createSubscription",
-      Membership: "/mint/createMembership",
-      "Menu Item": "/mint/createMenuItem",
-      Phygital: "/mint/createPhygital",
-    };
-    const route = routes[selectedOption];
-    if (route) {
-      window.location.href = route;
-    }
-  };
-
-  const handleOptionChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    setSelectedOption(event.target.value);
-  };
-
   return (
     <main className="main-container">
       <div className="bg-white p-4">
@@ -144,7 +118,9 @@ const MintDashboard = () => {
               {Object.entries(group.templatesByNftType).map(
                 ([nftType, templates]) => (
                   <div key={nftType}>
-                    <h3 className="text-xl font-semibold mb-2">{nftType}</h3>
+                    <h3 className="text-xl font-semibold mb-2">
+                      {capitalizeFirstLetter(nftType)}
+                    </h3>
                     <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 xl:gap-10">
                       {templates.map((template) => (
                         <MintCart
@@ -156,7 +132,14 @@ const MintDashboard = () => {
                           templateId={template.templateId}
                         />
                       ))}
-                      <div className="h-full w-full" onClick={handleSaveClick}>
+                      <div
+                        className="h-full w-full"
+                        onClick={() =>
+                          window.location.href = `/mint/create${capitalizeFirstLetter(
+                            nftType
+                          )}`
+                        }
+                      >
                         <SaveToLocalAndNavigate
                           collectionId={group.collection.id}
                         />
@@ -175,41 +158,6 @@ const MintDashboard = () => {
             </PushToMintCollectionButton>
           </Link>
         </div>
-
-        {isModalOpen && (
-          <div
-            className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
-            onClick={handleModalClose}
-          >
-            <div
-              className="bg-white p-6 rounded-lg w-1/3"
-              onClick={(e) => e.stopPropagation()}
-            >
-              <h3 className="text-xl font-bold mb-4">Select Type</h3>
-              <select
-                value={selectedOption}
-                onChange={handleOptionChange}
-                className="w-full mb-4 border border-gray-300 rounded-lg px-4 py-2"
-              >
-                <option value="" disabled>
-                  Select an option
-                </option>
-                <option value="Collectible">Collectible</option>
-                <option value="Subscription">Subscription</option>
-                <option value="Membership">Membership</option>
-                <option value="Coupon">Coupon</option>
-                <option value="Menu Item">Menu Item</option>
-                <option value="Phygital">Phygital</option>
-              </select>
-              <button
-                onClick={handleConfirmClick}
-                className="w-full bg-black text-white px-4 py-2 rounded-lg"
-              >
-                Confirm
-              </button>
-            </div>
-          </div>
-        )}
       </div>
     </main>
   );
