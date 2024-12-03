572c382cee1a5623c6ac51fab9dfebac98b220c3
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2024-12-03 14:55:27 +0600

    setting page loader added

--- app/(pages)/account-billing/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import SettingContent from '@/components/settings/SettingContent';
-
-const SettingPage: React.FC = () => {
-  return <SettingContent />;
-};
-
-export default SettingPage;
