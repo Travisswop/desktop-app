1513ca3f219ca5fa635018e534ecfe63a27bdae8
irakibul7 <irakibul568@gmail.com>
2025-01-06 16:38:39 +0600

    update xmtp chat

--- app/(pages)/(xmtp)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { XmtpProvider } from "@/lib/context/XmtpContext";
-
-const PageLayout = ({ children }: { children: React.ReactNode }) => {
-  return <XmtpProvider>{children}</XmtpProvider>;
-};
-
-export default PageLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import "./globals.css";
-import PrivyProvider from "@/components/PrivyProvider";
-import { UserProvider } from "@/lib/UserContext";
+import './globals.css';
+import PrivyProvider from '@/components/PrivyProvider';
+import { UserProvider } from '@/lib/UserContext';
 
-import { Figtree } from "next/font/google";
-import { TanstackProvider } from "@/components/providers/tanstackProvider";
-import { Toaster } from "react-hot-toast";
+import { Figtree } from 'next/font/google';
+import { TanstackProvider } from '@/components/providers/tanstackProvider';
+import { Toaster } from 'react-hot-toast';
+import { XmtpProvider } from '@/lib/context/XmtpContext';
 // import { XmtpProvider } from "@/lib/context/XmtpContext";
 
 // const roboto = Roboto({
@@ -12,8 +13,8 @@ import { Toaster } from "react-hot-toast";
 //   weight: ["100", "300", "400", "500", "700", "900"],
 // });
 const figtree = Figtree({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "700", "900"],
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '700', '900'],
 });
 
 export default function RootLayout({
@@ -27,9 +28,9 @@ export default function RootLayout({
         <Toaster position="top-center" reverseOrder={false} />
         <TanstackProvider>
           <PrivyProvider>
-            {/* <XmtpProvider> */}
-            <UserProvider>{children}</UserProvider>
-            {/* </XmtpProvider> */}
+            <XmtpProvider>
+              <UserProvider>{children}</UserProvider>
+            </XmtpProvider>
           </PrivyProvider>
         </TanstackProvider>
       </body>
