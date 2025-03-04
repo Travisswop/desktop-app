6b37f2399c452a427db1d21e41d15c615ee9d4ef
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2025-03-04 11:27:00 +0600

    update order

--- app/(public-profile)/sp/[username]/cart/Shipping.tsx
+++ app/(public-profile)/sp/[username]/cart/Shipping.tsx
@@ -1,13 +1,14 @@
-import AnimateButton from '@/components/ui/Button/AnimateButton';
-import { toast } from '@/components/ui/use-toast';
-import { truncateWalletAddress } from '@/lib/tranacateWalletAddress';
-import { useUser } from '@/lib/UserContext';
-import { TransactionService } from '@/services/transaction-service';
-import { useSolanaWallets } from '@privy-io/react-auth';
-import { clusterApiUrl, Connection } from '@solana/web3.js';
+import { createOrder } from "@/actions/order";
+import AnimateButton from "@/components/ui/Button/AnimateButton";
+import { toast } from "@/components/ui/use-toast";
+import { truncateWalletAddress } from "@/lib/tranacateWalletAddress";
+import { useUser } from "@/lib/UserContext";
+import { TransactionService } from "@/services/transaction-service";
+import { useSolanaWallets } from "@privy-io/react-auth";
+import { clusterApiUrl, Connection } from "@solana/web3.js";
 // import { AlertCircle } from "lucide-react";
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 // import { LiaFileMedicalSolid } from "react-icons/lia";
 // import { MdDelete } from "react-icons/md";
 
@@ -18,9 +19,9 @@ const PaymentShipping = ({
   amontOfToken,
   walletData,
 }: any) => {
-  const { user } = useUser();
-  const [address, setAddress] = useState('');
-  console.log('usersss', user);
+  const { user, accessToken } = useUser();
+  const [address, setAddress] = useState("");
+  console.log("usersss", user);
   const { createWallet, wallets: solanaWallets } = useSolanaWallets();
 
   useEffect(() => {
@@ -29,12 +30,14 @@ const PaymentShipping = ({
     }
   }, [user]);
 
+  console.log("user", user);
+
   const handleSendConfirm = async () => {
     try {
-      let hash = '';
+      let hash = "";
       let newTransaction;
       const recipientWallet = {
-        address: '4VoKLfzZNKQfmvitteM6ywtNNrdcikGuevkaTY1REhmN',
+        address: "4VoKLfzZNKQfmvitteM6ywtNNrdcikGuevkaTY1REhmN",
       };
       const sendFlow: any = {
         token: selectedToken,
@@ -42,20 +45,22 @@ const PaymentShipping = ({
         recipient: recipientWallet,
       };
 
-      console.log('sendFlow', sendFlow);
+      console.log("sendFlow", sendFlow);
 
       // const connection = new Connection(
       //   process.env.NEXT_PUBLIC_QUICKNODE_SOLANA_URL!,
       //   "confirmed"
       // );
 
-      const connection = new Connection(clusterApiUrl('devnet'));
+      const connection = new Connection(clusterApiUrl("devnet"));
 
-      console.log('connection', connection);
+      console.log("connection", connection);
 
       const solanaWallet = solanaWallets.find(
-        (w: any) => w.walletClientType === 'privy'
+        (w: any) => w.walletClientType === "privy"
       );
+
+      console.log("solanaWallet", solanaWallet);
 
       hash = await TransactionService.handleSolanaSend(
         solanaWallet,
@@ -64,17 +69,31 @@ const PaymentShipping = ({
       );
       await connection.confirmTransaction(hash);
 
-      console.log('hash', hash);
-      console.log('newTransaction', newTransaction);
+      const orderData = {
+        customerName: user?.name,
+        customerEmail: user?.email,
+        // customerPhone: userData?.countryCode + userData?.mobileNo,
+        customerPhone: "+01402348575",
+        customerShippingAddress: address,
+        txHash: hash,
+        customerWalletAddress: solanaWallet?.address,
+      };
+
+      if (hash && accessToken) {
+        const order = await createOrder(orderData, accessToken);
+
+        console.log("order", order);
+
+        console.log("hash", hash);
+        console.log("newTransaction", newTransaction);
+      }
     } catch (error) {
-      console.error('Error sending token/NFT:', error);
+      console.error("Error sending token/NFT:", error);
       toast({
-        variant: 'destructive',
-        title: 'Error',
+        variant: "destructive",
+        title: "Error",
         description:
-          error instanceof Error
-            ? error.message
-            : 'Failed to send transaction',
+          error instanceof Error ? error.message : "Failed to send transaction",
       });
       // resetSendFlow();
     }
@@ -85,7 +104,7 @@ const PaymentShipping = ({
       <div className="flex items-center gap-2 justify-between">
         <div className="flex items-center gap-2">
           <Image
-            src={'/astro-agent.png'}
+            src={"/astro-agent.png"}
             alt="astro"
             width={120}
             height={90}
@@ -94,7 +113,7 @@ const PaymentShipping = ({
           <div className="flex flex-col items-start">
             <p className="font-medium">Review</p>
             <p className="text-gray-500 font-medium">
-              Request from{' '}
+              Request from{" "}
               <a
                 href="http://swopme.co"
                 target="_blank"
@@ -109,12 +128,10 @@ const PaymentShipping = ({
         <h4 className="font-semibold text-gray-700">SWOP</h4>
       </div>
       <div className="bg-gray-200 p-3 flex flex-col items-start rounded">
-        <p className="text-gray-500 font-medium">
-          Asset Change (estimate)
-        </p>
+        <p className="text-gray-500 font-medium">Asset Change (estimate)</p>
         <p className="font-semibold">
           - <span className="text-red-500">{amontOfToken} </span>
-          {selectedToken.symbol ? selectedToken.symbol : 'SOL'}
+          {selectedToken.symbol ? selectedToken.symbol : "SOL"}
         </p>
       </div>
       <div className="flex items-center gap-2 justify-between">
@@ -133,9 +150,7 @@ const PaymentShipping = ({
       </div>
       <div className="flex items-center gap-2 justify-between">
         <p className="font-medium">Network</p>
-        <p className="text-gray-500 font-medium">
-          {selectedToken.chain}
-        </p>
+        <p className="text-gray-500 font-medium">{selectedToken.chain}</p>
       </div>
       <div className="flex items-center gap-2 justify-between">
         <p className="font-medium">Network Fee</p>
@@ -161,7 +176,7 @@ const PaymentShipping = ({
         <AnimateButton
           whiteLoading={true}
           className="w-full"
-          onClick={() => setSelectedToken('')}
+          onClick={() => setSelectedToken("")}
         >
           Cancel
         </AnimateButton>
