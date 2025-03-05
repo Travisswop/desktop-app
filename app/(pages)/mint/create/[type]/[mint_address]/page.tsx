d4a598cb6753855c8d0d3ca5347148ff3ead527a
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2025-03-05 10:25:39 +0600

    update

--- app/(pages)/mint/create/[type]/[mint_address]/page.tsx
+++ app/(pages)/mint/create/[type]/[mint_address]/page.tsx
@@ -1,16 +1,16 @@
-import CreateCollectible from '@/components/mint/collectible';
-import CreateCoupon from '@/components/mint/coupon';
-import CreateMembership from '@/components/mint/membership';
-import CreateMenu from '@/components/mint/menu';
-import CreatePhygital from '@/components/mint/phygital';
-import CreateSubscription from '@/components/mint/subscription';
+import CreateCollectible from "@/components/mint/collectible";
+import CreateCoupon from "@/components/mint/coupon";
+import CreateMembership from "@/components/mint/membership";
+import CreateMenu from "@/components/mint/menu";
+import CreatePhygital from "@/components/mint/phygital";
+import CreateSubscription from "@/components/mint/subscription";
 
 const COLLECTION_COMPONENTS = {
   collectible: CreateCollectible,
   coupon: CreateCoupon,
   membership: CreateMembership,
   menu: CreateMenu,
-  phygital: CreatePhygital,
+  phygitals: CreatePhygital,
   subscription: CreateSubscription,
 };
 
@@ -23,11 +23,7 @@ const CreateNFTTemplatePage = async ({ params }: Props) => {
 
   const Component =
     COLLECTION_COMPONENTS[type as keyof typeof COLLECTION_COMPONENTS];
-  return Component ? (
-    <Component collectionId={mint_address} />
-  ) : (
-    <></>
-  );
+  return Component ? <Component collectionId={mint_address} /> : <></>;
 };
 
 export default CreateNFTTemplatePage;
