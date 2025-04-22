d4427fb42feefe5a7c76e9e2e801e9d5dd3fdbe0
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2025-04-22 17:27:53 +0600

    update

--- components/smartsite/EditMicrosite/appIcon/AddAppIcon.tsx
+++ components/smartsite/EditMicrosite/appIcon/AddAppIcon.tsx
@@ -414,6 +414,7 @@ const AddAppIcon = ({ handleRemoveIcon }: any) => {
                 name="customName"
                 className="w-full border border-[#ede8e8] focus:border-[#e5e0e0] rounded-xl focus:outline-none pl-4 py-2 text-gray-700 bg-gray-100"
                 placeholder={"Enter name"}
+                defaultValue={"Custom Name"}
                 required
               />
             </div>
