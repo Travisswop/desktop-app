fdd15d89e6311ae47368e4f0e2f1656387f00467
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2025-07-30 15:47:54 +0600

    update metadata and og image for root

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,52 @@
-import './globals.css';
-import PrivyProvider from '@/components/PrivyProvider';
-import { UserProvider } from '@/lib/UserContext';
+import "./globals.css";
+import PrivyProvider from "@/components/PrivyProvider";
+import { UserProvider } from "@/lib/UserContext";
 
-import { Figtree, Inter } from 'next/font/google';
-import { Toaster } from 'react-hot-toast';
-import { XmtpProvider } from '@/lib/context/XmtpContext';
-import { WalletProvider } from '@/providers/SyncedWalletProvider';
-import { LiFiWalletProvider } from '@/providers/WalletManagementProvider';
+import { Figtree, Inter } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+import { XmtpProvider } from "@/lib/context/XmtpContext";
+import { WalletProvider } from "@/providers/SyncedWalletProvider";
+import { LiFiWalletProvider } from "@/providers/WalletManagementProvider";
+import { Metadata } from "next";
 
 const figtree = Figtree({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '700', '900'],
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700", "900"],
 });
 
 const inter = Inter({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-inter',
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-inter",
 });
+
+export const metadata: Metadata = {
+  title: "Swop Technologies",
+  description: "Web3 ecommerce",
+  metadataBase: new URL("https://www.swopme.app"),
+  openGraph: {
+    title: "Swop Technologies",
+    description: "Web3 ecommerce",
+    url: "https://www.swopme.app",
+    siteName: "Swop Technologies",
+    images: [
+      {
+        url: "/og-image.png", // Must be an absolute URL or path from public folder
+        width: 1200,
+        height: 630,
+        alt: "Swop Logo",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Swop Technologies",
+    description: "Web3 ecommerce",
+    images: ["/og-image.png"],
+  },
+};
 
 export default function RootLayout({
   children,
@@ -25,10 +54,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${figtree.className} ${inter.variable}`}
-    >
+    <html lang="en" className={`${figtree.className} ${inter.variable}`}>
       <body className={`font-[figtree] bg-[#F7F7F9]`}>
         <Toaster position="top-center" reverseOrder={false} />
 
