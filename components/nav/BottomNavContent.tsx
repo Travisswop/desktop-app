b60d2eb2e8e435f42583338b6a219634abb8b38d
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2025-10-07 12:46:25 +0600

    update in BottomNavContent component for checking conditional rendering for feed type content

--- components/nav/BottomNavContent.tsx
+++ components/nav/BottomNavContent.tsx
@@ -17,56 +17,58 @@ const BottomNavContent = () => {
 
   return (
     <div className="w-[19rem]  absolute bottom-2 left-1/2 transform -translate-x-1/2 ">
-      <div className="flex text-sm font-medium w-[84%] bg-white p-3 rounded-xl shadow-large items-center justify-between mb-2 mx-auto">
-        <button
-          onClick={() => setFeedType("feed")}
-          className="flex flex-col gap-1 items-center"
-        >
-          <div
-            className={`${
-              feedType === "feed" && "bg-gray-100"
-            } rounded-full px-3 py-1`}
+      {pathname === "/" && (
+        <div className="flex text-sm font-medium w-[84%] bg-white p-3 rounded-xl shadow-large items-center justify-between mb-2 mx-auto">
+          <button
+            onClick={() => setFeedType("feed")}
+            className="flex flex-col gap-1 items-center"
           >
-            <p>Feed</p>
-          </div>
-        </button>
-        <button
-          onClick={() => setFeedType("ledger")}
-          className="flex flex-col gap-1 items-center"
-        >
-          <div
-            className={`${
-              feedType === "ledger" && "bg-gray-100"
-            } rounded-full px-3 py-1`}
+            <div
+              className={`${
+                feedType === "feed" && "bg-gray-100"
+              } rounded-full px-3 py-1`}
+            >
+              <p>Feed</p>
+            </div>
+          </button>
+          <button
+            onClick={() => setFeedType("ledger")}
+            className="flex flex-col gap-1 items-center"
           >
-            <p>Ledger</p>
-          </div>
-        </button>
-        <button
-          onClick={() => setFeedType("map")}
-          className="flex flex-col gap-1 items-center"
-        >
-          <div
-            className={`${
-              feedType === "map" && "bg-gray-100"
-            } rounded-full px-3 py-1`}
+            <div
+              className={`${
+                feedType === "ledger" && "bg-gray-100"
+              } rounded-full px-3 py-1`}
+            >
+              <p>Ledger</p>
+            </div>
+          </button>
+          <button
+            onClick={() => setFeedType("map")}
+            className="flex flex-col gap-1 items-center"
           >
-            <p>Map</p>
-          </div>
-        </button>
-        <button
-          onClick={() => setFeedType("create-feed")}
-          className="flex flex-col gap-1 items-center"
-        >
-          <div
-            className={`${
-              feedType === "create-feed" && "bg-gray-100"
-            } rounded-full px-3 py-1`}
+            <div
+              className={`${
+                feedType === "map" && "bg-gray-100"
+              } rounded-full px-3 py-1`}
+            >
+              <p>Map</p>
+            </div>
+          </button>
+          <button
+            onClick={() => setFeedType("create-feed")}
+            className="flex flex-col gap-1 items-center"
           >
-            <BiSolidEdit size={18} />
-          </div>
-        </button>
-      </div>
+            <div
+              className={`${
+                feedType === "create-feed" && "bg-gray-100"
+              } rounded-full px-3 py-1`}
+            >
+              <BiSolidEdit size={18} />
+            </div>
+          </button>
+        </div>
+      )}
       <div className="flex w-full bg-white p-3 rounded-xl shadow-large items-center justify-between gap-2">
         <Link href={"/dashboard"} className="flex flex-col gap-1 items-center">
           <div className="bg-gray-100 rounded-lg p-3">
