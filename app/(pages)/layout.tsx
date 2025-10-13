9fdc9098ee5505170e8984217ec0b487ad4f7516
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2025-10-13 10:38:25 +0600

    Fix: 132

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -13,6 +13,7 @@ const PageLayout = ({ children }: { children: React.ReactNode }) => {
           <main className="container mx-auto px-6 py-6 max-w-7xl 2xl:max-w-full flex-1">
             {children}
           </main>
+          {/* use suspense to solve searchParams error  */}
           <Suspense fallback={""}>
             <BottomNavContent />
           </Suspense>
