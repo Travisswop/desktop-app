b5c6b71c724786f1ff5161287dcf7b21565d6424
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2025-12-28 12:51:32 +0600

    update login page

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -8,7 +8,6 @@ const FeedPage = () => {
       fallback={
         <div className="w-full sm:w-[520px] mx-auto">
           <FeedMainComponentLoading />
-          {/* <p>loading...</p> */}
         </div>
       }
     >
