7566f9dcf5405f45eba9c6db8ddc90aaf52d496e
irakibul7 <irakibul568@gmail.com>
2026-02-24 14:11:53 +0600

    fix transaction list bug

--- components/wallet/transaction/transaction-item.tsx
+++ components/wallet/transaction/transaction-item.tsx
@@ -1,6 +1,6 @@
-import { Transaction } from "@/types/transaction";
-import { useEffect, useState } from "react";
-import Image from "next/image";
+import { Transaction } from '@/types/transaction';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 interface TransactionItemProps {
   transaction: Transaction;
@@ -11,39 +11,44 @@ const truncateAddress = (address: string) => {
   return `${address.slice(0, 8)}.....${address.slice(-8)}`;
 };
 
-const TransactionItem = ({ transaction, onSelect }: TransactionItemProps) => {
-  const isOutgoing = transaction.flow === "out";
+const TransactionItem = ({
+  transaction,
+  onSelect,
+}: TransactionItemProps) => {
+  const isOutgoing = transaction.flow === 'out';
 
   const calculateValue = (value: string, price: number) => {
     const numericValue = parseFloat(value);
-    return (numericValue * price).toFixed(2);
+    return (numericValue * price).toFixed(4);
   };
 
   const getBorderColorByNetwork = (network: string) => {
     switch (network?.toLowerCase()) {
-      case "ethereum":
-        return "border-blue-400";
-      case "polygon":
-        return "border-purple-400";
-      case "solana":
-        return "border-green-400";
-      case "base":
-        return "border-cyan-400";
+      case 'ethereum':
+        return 'border-blue-400';
+      case 'polygon':
+        return 'border-purple-400';
+      case 'solana':
+        return 'border-green-400';
+      case 'base':
+        return 'border-cyan-400';
       default:
-        return "border-gray-400";
+        return 'border-gray-400';
     }
   };
 
   const [borderClass, setBorderClass] = useState(
     transaction?.isNew
       ? `border-2 ${getBorderColorByNetwork(transaction.network)} animate-pulse`
-      : "border border-gradient-to-r from-gray-200 to-gray-300"
+      : 'border border-gradient-to-r from-gray-200 to-gray-300',
   );
 
   useEffect(() => {
     if (transaction?.isNew) {
       const timer = setTimeout(() => {
-        setBorderClass("border border-gradient-to-r from-gray-200 to-gray-300");
+        setBorderClass(
+          'border border-gradient-to-r from-gray-200 to-gray-300',
+        );
       }, 10000);
 
       return () => clearTimeout(timer);
@@ -63,7 +68,7 @@ const TransactionItem = ({ transaction, onSelect }: TransactionItemProps) => {
               <div className="w-6 h-6 rounded-full overflow-hidden">
                 <Image
                   src={`/assets/crypto-icons/${transaction.swapped?.from.symbol}.png`}
-                  alt={transaction.swapped?.from.symbol || ""}
+                  alt={transaction.swapped?.from.symbol || ''}
                   width={32}
                   height={32}
                   className="object-cover"
@@ -78,7 +83,7 @@ const TransactionItem = ({ transaction, onSelect }: TransactionItemProps) => {
               <div className="w-6 h-6 rounded-full overflow-hidden -ml-2">
                 <Image
                   src={`/assets/crypto-icons/${transaction.swapped?.to.symbol}.png`}
-                  alt={transaction.swapped?.to.symbol || ""}
+                  alt={transaction.swapped?.to.symbol || ''}
                   width={32}
                   height={32}
                   className="object-cover"
@@ -92,8 +97,8 @@ const TransactionItem = ({ transaction, onSelect }: TransactionItemProps) => {
             <div>
               <p className="font-semibold">Swapped</p>
               <p className="text-sm text-muted-foreground">
-                {transaction.swapped?.from.symbol} <span>&#x2192;</span>{" "}
-                {transaction.swapped?.to.symbol}
+                {transaction.swapped?.from.symbol}{' '}
+                <span>&#x2192;</span> {transaction.swapped?.to.symbol}
               </p>
             </div>
           </>
@@ -102,7 +107,7 @@ const TransactionItem = ({ transaction, onSelect }: TransactionItemProps) => {
             <div className="w-6 h-6 rounded-full overflow-hidden">
               <Image
                 src={`/assets/crypto-icons/${transaction.tokenSymbol}.png`}
-                alt={transaction.tokenSymbol || ""}
+                alt={transaction.tokenSymbol || ''}
                 width={40}
                 height={40}
                 className="object-cover"
@@ -114,13 +119,15 @@ const TransactionItem = ({ transaction, onSelect }: TransactionItemProps) => {
             </div>
             <div>
               <p className="font-semibold">
-                {isOutgoing ? "Sent" : "Received"}
+                {isOutgoing ? 'Sent' : 'Received'}
               </p>
               <p className="text-sm text-muted-foreground max-w-[200px] md:max-w-[300px]">
                 {isOutgoing ? (
                   <span>To {truncateAddress(transaction.to)}</span>
                 ) : (
-                  <span>From {truncateAddress(transaction.from)}</span>
+                  <span>
+                    From {truncateAddress(transaction.from)}
+                  </span>
                 )}
               </p>
             </div>
@@ -130,11 +137,12 @@ const TransactionItem = ({ transaction, onSelect }: TransactionItemProps) => {
       <div className="text-right">
         <p
           className={`font-medium ${
-            isOutgoing ? "text-red-600" : "text-green-600"
+            isOutgoing ? 'text-red-600' : 'text-green-600'
           }`}
         >
-          {isOutgoing ? "-" : "+"}
-          {parseFloat(transaction.value).toFixed(2)} {transaction.tokenSymbol}
+          {isOutgoing ? '-' : '+'}
+          {parseFloat(transaction.value).toFixed(4)}{' '}
+          {transaction.tokenSymbol}
         </p>
         <p className="text-sm text-muted-foreground">
           {transaction.isSwapped ? (
@@ -142,12 +150,12 @@ const TransactionItem = ({ transaction, onSelect }: TransactionItemProps) => {
               $
               {calculateValue(
                 transaction.swapped!.from.value,
-                transaction.swapped!.from.price
+                transaction.swapped!.from.price,
               )}
-              {" → "}$
+              {' → '}$
               {calculateValue(
                 transaction.swapped!.to.value,
-                transaction.swapped!.to.price
+                transaction.swapped!.to.price,
               )}
             </>
           ) : (
