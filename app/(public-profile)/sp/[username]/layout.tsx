e194a60eb9044b2cbb75bac9a1d6c5caecfea5bb
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2026-05-25 10:18:49 +0600

    update token gated for public smartsite

--- app/(public-profile)/sp/[username]/layout.tsx
+++ app/(public-profile)/sp/[username]/layout.tsx
@@ -1,22 +1,22 @@
-import { ReactNode } from 'react';
-import { MicrositeProvider } from './context/MicrositeContext';
-import { getUserData } from '@/actions/user';
+import { ReactNode } from "react";
+import { MicrositeProvider } from "./context/MicrositeContext";
+import { getUserData } from "@/actions/user";
+import QueryProvider from "@/components/provider/QueryProvider";
 
 interface LayoutProps {
   children: ReactNode;
   params: Promise<{ username: string }>;
 }
 
-export default async function Layout({
-  children,
-  params,
-}: LayoutProps) {
+export default async function Layout({ children, params }: LayoutProps) {
   const userName = (await params)?.username;
   const { data } = await getUserData(userName);
 
   return (
-    <MicrositeProvider micrositeData={data?.microsite ?? null}>
-      {children}
-    </MicrositeProvider>
+    <QueryProvider>
+      <MicrositeProvider micrositeData={data?.microsite ?? null}>
+        {children}
+      </MicrositeProvider>
+    </QueryProvider>
   );
 }
